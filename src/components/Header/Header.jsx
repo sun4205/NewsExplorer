34935f683f4cca031ec995a8899e43363f87a076
sun4205 <hanna4205@gmail.com>
2025-03-10 14:13:04 -0400

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../images/logo.jpg";
+
+
+function Header() {
+  return (
+    <div className="header__container">
+    <header className="header">
+      <img src={logo} alt={logo} class="header__logo" />
+      <div className="header__nav">
+        <button className="header__homge-btn">Home</button>
+        <button className="header__signIn-btn">Sign In</button>
+      </div>
+    </header>
+    </div>
+  );
+}
+
+export default Header;
