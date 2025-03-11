a03c54ee7e0775972214d185dba9ee2678d543bc
sun4205 <hanna4205@gmail.com>
2025-03-11 14:28:56 -0400

    add Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,17 @@
-import './Main.css';
-import NewsCard from '../NewsCard/NewsCard';
+import "./Main.css";
+import NewsCard from "../NewsCard/NewsCard";
 
-function Main(){
-    return(
-        <main className='main'>
-            <h2 className='main__title'>Search results</h2>
-            <NewsCard />
-
-        </main>
-    )
+function Main() {
+  return (
+    <main className="main">
+      <h2 className="main__title">Search results</h2>
+      <section className="cards">
+        <ul className="cards__list">
+          <NewsCard />
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
