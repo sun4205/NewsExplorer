6a94571363ed9cbd1e3f1d2756255918529a3e64
sun4205 <hanna4205@gmail.com>
2025-03-11 20:07:32 -0400

    add token

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,79 +3,76 @@ const backendBaseUrl = "http://localhost:3001";
 const API_KEY = "MY_API_KEY";
 
 function checkResponse(res) {
-    return res.ok? res.json() : Promise.reject(`Error:${res.status}`);    
+  return res.ok ? res.json() : Promise.reject(`Error:${res.status}`);
 }
 
-function request(url,options){
- return fetch(url,options).then(checkResponse);
+function request(url, options) {
+  return fetch(url, options).then(checkResponse);
 }
 
 function getNewsCards(query) {
-    return request(`${newsApiBaseUrl}?q=${query}&apiKey=${API_KEY}`);
+  return request(`${newsApiBaseUrl}?q=${query}&apiKey=${API_KEY}`);
 }
 
-function savedNews({source, title, date, description, image}) {
-    return request(`${backendBaseUrl}/savedNews`,{
-        method:"POST",
-        headers: {
-            "Content-Type":"application/json",
-            authorization:`Bearer ${localStorage.getItem("jwt")}`
-        },
-        body:JSON.stringify({
-            source,
-            title,
-            date,
-            description,
-            image,
-        }),
-    });
-};
-
+function savedNews({ source, title, date, description, image }) {
+  const token = localStorage.getItem("jwt");
+  return request(`${backendBaseUrl}/savedNews`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      source,
+      title,
+      date,
+      description,
+      image,
+    }),
+  });
+}
 
 const deleteNewsCard = (_id) => {
-    const token = localStorage.getItem("jwt");
-    console.log("Deleting NewsCard with _id:", _id);
-    return request(`${backendBaseUrl}/savedNews/${_id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-  };
-    
-  
-  const NewsCardSaved = (id, token) => {
-    console.log("Card ID:", id);
-    return fetch(`${backendBaseUrl}/saveNews/${id}/saved`, {
-      method: "PUT",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .catch((err) => console.log(err));
-  };
-  
-  const removeNewsCardSved = (id, token) => {
-    return fetch(`${backendBaseUrl}/saveNews/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .catch((err) => console.log(err));
-  };
-  
-  export {    
-    checkResponse,
-    getNewsCards,
-    savedNews,
-    deleteNewsCard,
-    NewsCardSaved,
-    removeNewsCardSved
-  };
-  
+  console.log("Deleting NewsCard with _id:", _id);
+  return request(`${backendBaseUrl}/savedNews/${_id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const NewsCardSaved = (id, token) => {
+  console.log("Card ID:", id);
+  return fetch(`${backendBaseUrl}/saveNews/${id}/saved`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+
+const removeNewsCardSved = (id, token) => {
+  return fetch(`${backendBaseUrl}/saveNews/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+
+export {
+  checkResponse,
+  getNewsCards,
+  savedNews,
+  deleteNewsCard,
+  NewsCardSaved,
+  removeNewsCardSved,
+};
