81987daf456652268451b9e4e686df108a4df91e
sun4205 <hanna4205@gmail.com>
2025-03-12 11:18:26 -0400

    add news card

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -9,10 +9,12 @@ function NewsCard(data) {
     <li className="card">
       <img className="card__image" src={data.image} alt={data.image} />
       <button className="card__save-btn"></button>
+      <div className="card__info">
       <p className="card__date">{currentDate}</p>
       <p className="card__title">{data.title}</p>
       <p className="card__description">{data.description}</p>
       <p className="card__source">{data.source}</p>
+      </div>
     </li>
   );
 }
