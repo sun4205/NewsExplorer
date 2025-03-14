a8626734c08238dd6a97cc166375b871b91acdfc
sun4205 <hanna4205@gmail.com>
2025-03-14 11:27:40 -0400

    add navgation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,40 @@
 import './Navigation.css';
+import Union from "../../images/Union.svg";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-function Navigation() {
+function Navigation(openLoginModal) {
+    const { currentUser } = useContext(CurrentUserContext);
     return(
-        <></>
+        <div className="navigation__nav">
+        <button type="button" className="navigation__home-btn">
+          Home
+        </button>
+        {!currentUser ? (
+          <button
+            type="button"
+            onClick={openLoginModal}
+            className="navigation__signIn-btn"
+          >
+            Sign In
+          </button>
+        ) : (
+          <div className="navigation__loggedIn-control">
+            <a href="/savedArticle">
+              <button type="button" className="navigation__savedArticle-nav">
+                Saved Articles
+              </button>
+            </a>
+           
+              <div className="navigation__username">{currentUser.username}
+                <button type="button">
+                  <img src ={Union} className="navigation__logout"/>
+                </button>
+              </div>
+            
+          </div>
+        )}
+      </div>
     )
 }
 
