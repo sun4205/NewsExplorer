0c43e4dbc32db592b301f0e6f2ae188f06896507
sun4205 <hanna4205@gmail.com>
2025-03-13 22:09:07 -0400

    update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,25 @@
-import './About.css';
-import me from '../../images/hanna.png';
+import "./About.css";
+import me from "../../images/hanna.png";
 
 function About() {
-    return(
-        <div className='about'>
-            <img src={me} alt={me} class='about__mypicture'/>
-            <div className='about__author'>
-                <h2 className='about__author-title'>About the Author</h2>
-                <p className='about__author-description1'>I am a Full Stack Engineer, transitioning from 10 years as a Dental Technician. I have strong skills in meeting deadlines, attention to detail, and teamwork.</p>
-                    <p className='about__author-description2'>At TripleTen, I learned React, Node.js, and JavaScript to build web solutions. Now, I'm ready to use my skills to help customers by creating efficient and user-friendly applications.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="about">
+      <img src={me} alt={me} class="about__mypicture" />
+      <div className="about__author">
+        <h2 className="about__author-title">About the Author</h2>
+        <p className="about__author-description1">
+          I am a Full Stack Engineer, transitioning from 10 years as a Dental
+          Technician. I have strong skills in meeting deadlines, attention to
+          detail, and teamwork.
+        </p>
+        <p className="about__author-description2">
+          At TripleTen, I learned React, Node.js, and JavaScript to build web
+          solutions. Now, I'm ready to use my skills to help customers by
+          creating efficient and user-friendly applications.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
