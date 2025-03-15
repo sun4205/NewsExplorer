a9bffa79cce8bc86c1ae1b7b5ec7266c0ff5c25f
sun4205 <hanna4205@gmail.com>
2025-03-15 15:35:46 -0400

    update main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,17 @@ import "./Main.css";
 import NewsCard from "../NewsCard/NewsCard";
 import About from "../About/About";
 
+
 function Main() {
   return (
     <main className="main">
       <h2 className="main__title">Search results</h2>
       <section className="main__cards">
         <ul className="main__cards-list">
-          <NewsCard />
+          <NewsCard />          
         </ul>
         <About />
+       
       </section>
     </main>
   );
