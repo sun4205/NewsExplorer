d203b33d858e1b28e4ef938087c54120f553a421
sun4205 <hanna4205@gmail.com>
2025-03-17 14:29:04 -0400

    update auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,32 +2,31 @@ import { checkResponse } from "./api";
 export const baseUrl = "http://localhost:3001";
 
 export const register = (email, password, username) => {
-    return fetch(`${baseUrl}/signup`, {
-      method: "POST",
-      headers: {        
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password, username}),
-    }).then(checkResponse);
-  };
-  
-  export const authorize = (email, password) => {
-    return fetch(`${baseUrl}/signin`, {
-      method: "POST",
-      headers: {       
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    }).then(checkResponse);
-  };
-  
-  export const getUserInfo = (token) => {
-    return fetch(`${baseUrl}/users/me`, {
-      method: "GET",
-      headers: {        
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(checkResponse);
-  };
-  
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, username }),
+  }).then(checkResponse);
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const getUserInfo = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
