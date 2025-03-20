9a99ab51511580a45dc3cb490e681ea672ec4706
sun4205 <hanna4205@gmail.com>
2025-03-20 17:41:43 -0400

    add newscard map

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,13 +4,14 @@ import About from "../About/About";
 import ShowMore from "../ShowMore/ShowMore";
 
 
-function Main() {
+function Main({newData, newsItems}) {
   return (
     <main className="main">
       <h2 className="main__title">Search results</h2>
       <section className="main__cards">
         <ul className="main__cards-list">
-          <NewsCard />          
+          {newsItems.slice(0,9).map((item,index)=>(<NewsCard key={index} data={item}/>   ))}
+                 
         </ul>
         <ShowMore />
         <About />
