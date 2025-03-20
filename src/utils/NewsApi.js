b19b902016f026891df363bb5af8314a5b8dbb72
sun4205 <hanna4205@gmail.com>
2025-03-20 17:42:24 -0400

    update newsapi

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,30 +1,34 @@
 import { checkResponse } from "./api";
 
-// export const newsApiBaseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://nomoreparties.co/news/v2/everything"
-//     : "https://newsapi.org/v2/everything";
-
-    export const newsApiBaseUrl =
+export const newsApiBaseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://nomorepart.co/news/v2/everything"
-    : "https://news.org/v2/everything";
+    ? "https://nomoreparties.co/news/v2/everything"
+    : "https://newsapi.org/v2/everything";
+
+  //   export const newsApiBaseUrl =
+  // process.env.NODE_ENV === "production"
+  //   ? "https://nomorepart.co/news/v2/everything"
+  //   : "https://news.org/v2/everything";
 
 export const getNewsCards = (query) => {
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  console.log("API_KEY", API_KEY);
+  const API_KEY = import.meta.env.VITE_API_KEY;  
   return fetch(`${newsApiBaseUrl}?q=${query}&apiKey=${API_KEY}`).then(
     checkResponse
   );
 };
 
 export const filteredNewsData = (data) => {
-  const result = {};
-  result.source = data.source.name;
-  result.title = data.title;
-  result.date = data.publishedAt;
-  result.description = data.description;
-  result.image = data.urlToImage;
-  console.log(result);
-  return result;
+  if (!data?.articles || !Array.isArray(data.articles) || data.articles.length === 0) {
+    console.error(" No articles found");
+    return [];
+  }
+  
+  return data.articles.map((article)=>({
+    source : article?.source?.name || "unknown source",
+    title : article?.title || "no title",
+    date : article?.publishedAt || " no date",
+    description : article?.description || " no description",
+    image : article?.urlToImage || "no image"
+
+  }))
 };
