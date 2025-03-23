3b81c2ec6b3f3f1ee311cc2c48f83e94c2229135
sun4205 <hanna4205@gmail.com>
2025-03-22 21:33:57 -0400

    newscard showing update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,9 +2,15 @@ import "./Main.css";
 import NewsCard from "../NewsCard/NewsCard";
 import About from "../About/About";
 import ShowMore from "../ShowMore/ShowMore";
+import { useState } from "react";
 
 function Main({ newsData, newsItems, handleNewsSaved }) {
+  const [renderedCards, setRenderedCards] = useState(3);
   console.log("newsItems in Main:", newsItems);
+
+  const handleShowMore = () => {
+    setRenderedCards((prev)=> Math.min(prev + 3, 9));
+  }
   return (
     <main className="main">
       <h2 className="main__title">Search results</h2>
@@ -13,7 +19,7 @@ function Main({ newsData, newsItems, handleNewsSaved }) {
           {Array.isArray(newsItems.articles) &&
           newsItems.articles.length > 0 ? (
             newsItems.articles
-              .slice(0, 9)
+              .slice(0, renderedCards)
               .map((item, index) => (
                 <NewsCard
                   key={index}
@@ -22,10 +28,12 @@ function Main({ newsData, newsItems, handleNewsSaved }) {
                 />
               ))
           ) : (
-            <p>No news items available</p>
+            <p>Nothing found</p>
           )}
         </ul>
-        <ShowMore />
+        {renderedCards < 9 && renderedCards < newsItems.articles.length && (
+          <ShowMore onClick={handleShowMore} />
+        )}
         <About />
       </section>
     </main>
