fcfcce6878049176c813de026f6da4c4da459d33
sun4205 <hanna4205@gmail.com>
2025-04-07 14:34:18 -0400

    update mobie

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -17,33 +17,62 @@ function SearchForm({ handleSearchSubmit, query, setQuery }) {
   };
   // 
   return (
+    // <div className="searchForm__container">
+    //   <h1 className="searchForm__title">What's going on in the world?</h1>
+    //   <p className="searchForm__description">
+    //     Find the latest news on any topic and save them in your personal
+    //     account.
+    //   </p>
+    //   <div className="searchForm__controls">
+    //     <label className="searchForm__label">
+    //       <input
+    //         className="searchForm__input"
+    //         type="search"
+    //         id="search__input"
+    //         name="search__input"
+    //         placeholder="Enter Topic"
+    //         value={query}
+    //         onChange={handleInputChange}
+    //       ></input>
+    //     </label>
+    //     <button
+    //       type="button"
+    //       onClick={handleSubmit}
+    //       className="searchForm__btn"
+    //     >
+    //       Search
+    //     </button>
+    //   </div>
+    // </div>
     <div className="searchForm__container">
-      <h1 className="searchForm__title">What's going on in the world?</h1>
-      <p className="searchForm__description">
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
-      <div className="searchForm__controls">
-        <label className="searchForm__label">
-          <input
-            className="searchForm__input"
-            type="search"
-            id="search__input"
-            name="search__input"
-            placeholder="Enter Topic"
-            value={query}
-            onChange={handleInputChange}
-          ></input>
+     <h1 className="searchForm__title">What's going on in the world?</h1>
+     <p className="searchForm__description">
+      Find the latest news on any topic and save them in your personal
+      account.
+     </p>
+     <div className="searchForm__controls">
+       <label className="searchForm__label">
+        <input
+          className="searchForm__input"
+          type="search"
+          id="search__input"
+          name="search__input"
+          placeholder="Enter Topic"
+          value={query}
+          onChange={handleInputChange}
+        ></input>
         </label>
         <button
-          type="button"
-          onClick={handleSubmit}
-          className="searchForm__btn"
+        type="button"
+        onClick={handleSubmit}
+        className="searchForm__btn"
         >
-          Search
+        Search
         </button>
       </div>
     </div>
+    
+  
   );
 }
 
