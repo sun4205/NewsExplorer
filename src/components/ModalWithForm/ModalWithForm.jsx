0c77eb7be300245e99096ca205cf843f0b1f8b03
sun4205 <hanna4205@gmail.com>
2025-04-09 21:57:29 -0400

    update modal

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -20,9 +20,10 @@ function ModalWithForm({
 
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`} ref={modalRef}>
+      
       <div className="modal__content">
-        <h2 className="modal__title">{title}</h2>
-        <button
+      <h2 className="modal__title">{title}</h2>
+      <button
           onClick={() => {
             console.log("closeActiveModal called");
             closeActiveModal();
@@ -30,8 +31,9 @@ function ModalWithForm({
           type="button"
           className="modal__close"
         >
-          {/* <img src={close} className="modal__close-btn" alt="close_button" /> */}
-        </button>
+          <img src={close} className="modal__close-btn" alt="close_button" />
+        </button>        
+        
         <form onSubmit={onSubmit} className="modal__form">
           {children}
           <div className="modal__button-container">
