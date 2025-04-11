b6592a5185803ae639ffab0c2c667cac868ecfed
sun4205 <hanna4205@gmail.com>
2025-04-11 16:16:50 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NewsExplorer/',
   plugins: [react()],
   server: {
     port: 3001,
