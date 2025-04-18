e743c35675575351556c75b63174f7e9707a7fbf
sun4205 <hanna4205@gmail.com>
2025-04-18 12:47:00 -0400

    update about  searchform

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,24 +4,26 @@ import me from "../../images/hanna.png";
 function About({ query }) {
   return (
     <>
-    <div className="about">
-      <img src={me} alt="author Picture" className="about__mypicture" />
-      <div className="about__author">
-        <h2 className="about__author-title">About the Author</h2>
-        <div className="about__author-descriptions">
-        <p className="about__author-description1">
-          I am a Full Stack Engineer, transitioning from 10 years as a Dental
-          Technician. I have strong skills in meeting deadlines, attention to
-          detail, and teamwork.
-        </p>
-        <p className="about__author-description2">
-          At TripleTen, I learned React, Node.js, and JavaScript. Now, I'm ready to use my skills to help customers by
-          creating efficient and user-friendly applications.
-        </p>
+      <section className="about">
+        <div className="about__wrapper">
+          <img src={me} alt="author" className="about__mypicture" />
+          <div className="about__author">
+            <h2 className="about__author-title">About the Author</h2>
+            <div className="about__author-descriptions">
+              <p className="about__author-description1">
+                I am a Full Stack Engineer, transitioning from 10 years as a Dental
+                Technician. I have strong skills in meeting deadlines, attention to
+                detail, and teamwork.
+              </p>
+              <p className="about__author-description2">
+                At TripleTen, I learned React, Node.js, and JavaScript. Now, I'm ready to use my skills to help customers by
+                creating efficient and user-friendly applications.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    {!query && <div className="about__spacer"></div>}
+      </section>
+      {!query && <div className="about__spacer"></div>}
     </>
   );
 }
